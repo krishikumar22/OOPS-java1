dc97535474c8da87282360e7a47b0c7a826226f7
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-06 09:11:57 +0530

    Add files via upload

---
+++ ThisKeyword.java
@@ -0,0 +1,20 @@
+class K
+{
+	int num1;
+	int num2;
+	
+	public K(int num1,int num2)
+	{
+		this.num1=num1;
+		this.num2=num2;
+	}
+}
+public class ThisKeyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+A obj=new A();
+System.out.println(obj.num1);
+	}
+
+}
