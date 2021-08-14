6545f392df32c7ed757597832f76d738a9887d13
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-14 21:23:42 +0530

    Add files via upload

---
+++ Arrayjagged.java
@@ -0,0 +1,25 @@
+
+public class Arrayjagged {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {1,3,5,7};
+		int b[]= {2,4,6,8};
+	int c[]= {10,20,30,40};
+	int d[][]= {
+			{1,3,5,7},
+			{2,64,6},
+			{10,20,30,40,70,90}
+	};
+	for (int i=0;i<d.length;i++)
+	{
+		for(int j=0;j<d[i].length;j++) {
+			System.out.print(" "+d[i][j]);
+		}
+		System.out.println();
+	}
+	}
+	
+	}
+
+
