3082088296688c3e8b77801da0a9c441436b8987
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-16 10:27:27 +0530

    Add files via upload

---
+++ FinalKeywordDemo.java
@@ -0,0 +1,19 @@
+final class Op{
+	public final void Week() {
+		final int DAY;
+		DAY=10;
+	
+	}
+}
+
+
+
+public class FinalKeywordDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Op obj=new Op();
+obj.Week();
+	}
+
+}
