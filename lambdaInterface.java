9346ddfc0ccd4fc6bab89f6f211b1feee7f255bb
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-16 10:28:22 +0530

    Add files via upload

---
+++ lambdaInterface.java
@@ -0,0 +1,12 @@
+interface Calm{
+	public void read();
+}
+public class lambdaInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Calm obj =()->System.out.println("Im the best");
+obj.read();
+	}
+
+}
